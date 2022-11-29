7846e3bbf0e6ce92d040fcacc2bf57786da6ad50
shoot-h <81802152+shoot-h@users.noreply.github.com>
2022-11-29 18:05:44 +0900

    Add files via upload

---
+++ readfile.py
@@ -0,0 +1,74 @@
+import javalang
+import re
+from collections import Counter
+from torchtext.vocab import vocab
+
+
+
+def readf(a,b):
+    str1 = []
+    str2 = []
+    for i in range(a,b):#32294
+        try:
+            f1 = open('CoDas/CoDas_{:0>5}.java'.format(i), 'r')
+            f2 = open('CoDas/CoDas_{:0>5}.class.txt'.format(i), 'r')
+
+            data1 = f1.read()
+            data2 = f2.read()
+            str1.append(data1)
+            str2.append(data2)
+            f1.close()
+            f2.close()
+        except:
+                pass
+    
+    print(len(str1))
+
+    return str1, str2
+
+def readfs(a,b):
+    for i in range(a,b):
+        try:
+            f1 = open('CoDas/CoDas_{:0>5}.java'.format(1), 'r')
+            f2 = open('CoDas/CoDas_{:0>5}.class.txt'.format(1), 'r')
+
+            data1 = f1.read()
+            data2 = f2.read()
+            str1.append(data1)
+            str2.append(data2)
+            f1.close()
+            f2.close()
+        except:
+                pass
+
+    return str1, str2
+
+def texttokenize(str):
+    return re.findall(".*?[\t\n]", str)
+
+def srctokenize(str):
+    tokensrc = list(javalang.tokenizer.tokenize(str))
+    tokens = []
+    for i in range (len(tokensrc)):
+        tokens.append(tokensrc[i].value)
+    return tokens
+
+def build_srcvocab(texts):
+    
+    counter = Counter()
+    for text in texts:
+        counter.update(srctokenize(text))
+    return vocab(counter, specials=['<unk>', '<pad>', '<start>', '<end>'])
+
+def build_textvocab(texts):
+    
+    counter = Counter()
+    for text in texts:
+        counter.update(texttokenize(text))
+    return vocab(counter, specials=['<unk>', '<pad>', '<start>', '<end>'])
+
+
+
+#print(texttokenize(str2[0]))
+
+#print(srctokenize(str1[0]))
